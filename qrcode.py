9dbb49531d52ce2d6f752a688bdc22976454e032
Tymoteusz Niewodniczanski <tymekwn@gmail.com>
2021-10-02 12:22:28 +0100

    First Version, changing colours based on URL.

---
+++ qrcode.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/qr")
+def colour():
+    with open("input.txt","r") as f:
+        color = f.read()
+
+    return render_template("base.html",color=color)
+
+
+@app.route("/house/<house>")
+def write_colour(house):
+    house_dict = {
+        "scott":"c01928",
+        "etheldreda":"f0c566",
+        "franklin":"009640",
+        "turing":"1374bf",
+        "seacole":"7a348a"
+    }
+    
+    with open("input.txt", "w") as f:
+        f.write(house_dict[house])
+
+    return f"Thank you for choosing the {house} house!" 
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
